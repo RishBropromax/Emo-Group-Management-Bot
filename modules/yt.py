b3062fd7f5b675d8617cc4fb24c2edb662f04333
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-06-03 16:02:52 +0530

    Create yt.py

---
+++ modules/yt.py
@@ -0,0 +1,6 @@
+__help__ = """
+@Emo_Deckstop_Bot
+ ❍ /yt <text>: perform a youtube search
+ ❍ /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+"""
+__mod_name__ = "Youtube"
