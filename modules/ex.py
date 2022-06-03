f6b53a4799b6396075aaa3ac3b9734ad93dfb8d5
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-06-03 14:50:07 +0530

    Create ex.py

---
+++ modules/ex.py
@@ -0,0 +1,8 @@
+__help__ =  """
+@ImRishmika
+** Telegraph ** 
+  ❍ tm :Get Telegraph Link Of Replied Media
+  ❍ txt :Get Telegraph Link of Replied Text
+ I can upload files to Telegraph
+"""
+__mod_name__ = "Telegraph"       
